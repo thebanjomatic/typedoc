fdce4bfdd06a3730599d360f2521829f6623577b
Adam J. Hines <ahines@factset.com>
2017-11-03 18:31:51 -0400

    chore(version) typescript 2.6.1

---
+++ dist/lib/utils/options/sources/component.d.ts
@@ -0,0 +1,8 @@
+import { OptionsComponent } from '../options';
+export declare class ComponentSource extends OptionsComponent {
+    private knownComponents;
+    protected initialize(): void;
+    private addComponent(component);
+    private onComponentAdded(e);
+    private onComponentRemoved(e);
+}
